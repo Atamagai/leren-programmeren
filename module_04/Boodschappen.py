a4a6ce8cf1112b144e83bfa8b907c59048ebe6ec
Atamagai <99071577@mydavinci.nl>
2022-11-28 14:12:45 +0100

    Update Boodschappen.py

--- module_04/Boodschappen.py
+++ module_04/Boodschappen.py
@@ -1,33 +1,35 @@
 print("Welkom")
 lijst = {}
 
-item = input("Wat wil je aan je boodschappen lijst toevoegen? ").lower()
-aantal = int(input("Hoeveel wil je daarvan? "))
-lijst[item] = aantal
+
 
 repeat = True
 
 while repeat == True:
+    item = input("Wat wil je aan je boodschappen lijst toevoegen? ").lower()
+    aantal = int(input("Hoeveel wil je daarvan? "))
+
+    # print(list(lijst.keys()))
+    
+    if item in lijst:
+        lijst[item] = lijst[item] + aantal
+    else:
+        lijst[item] = aantal
+
     herhaal = input("Will je nog iets toevoegen? ").lower()
 
     if herhaal == "ja":
-        item = input("Wat wil je nog aan je boodschappen lijst toevoegen? ").lower()
-        
-        aantal1 = int(input("Hoeveel wil je daarvan? "))
-        if item in lijst:
-            aantal = aantal1 + aantal
-            lijst.update({item : aantal})
-        else:
-            lijst[item] = aantal
-        repeat = True 
+        repeat = True
 
     elif herhaal == "nee":
         repeat = False
     
-lijst[item] = aantal
-
 print("\nHier is jou lijst:")
 print("---------------------------")
 for key, value in lijst.items():
     print(value,"x ",key)
 print("---------------------------")
+
+
+
+
